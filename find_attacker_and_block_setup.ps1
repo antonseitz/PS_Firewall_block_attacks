718925de679bcd7b5a6ba33c134e0e3b12b5d230
antonseitz <aseitz@puckit.de>
2021-11-24 19:39:10 +0100

    FIX

--- find_attacker_and_block_setup.ps1
+++ find_attacker_and_block_setup.ps1
@@ -62,5 +62,5 @@ $D = New-ScheduledTask -Action $A  -Trigger $T -Settings $S
 Register-ScheduledTask $taskname -InputObject $D -taskpath "\" -user $user -pass $pass
 
 
-$taskname + " is scheduled to run every 15 minutes""
+$taskname + " is scheduled to run every 15 minutes"
 
